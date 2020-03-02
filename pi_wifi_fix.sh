dc4bdffaebc48fad368e2fd635297e589d867bdf
zulrosli96 <42715867+zulrosli96@users.noreply.github.com>
2020-03-02 15:46:59 +0800

    Add files via upload

---
+++ pi_wifi_fix.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo mount -o remount,rw / | ifconfig wlan0 down | sudo apt-get install wireless-tools | cd /lib/firmware/ | mkdir brcm | cd brcm | wget wget --no-check-certificate https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm/brcmfmac43430-sdio.bin | wget --no-check-certificate https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/master/brcm/brcmfmac43430-sdio.txt | ifconfig wlan0 up | reboot
